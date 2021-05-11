c8e907bf66d52d6a67eb246d4ba7d87d18452874
rqsell <68514875+rqsell@users.noreply.github.com>
2021-05-11 17:25:53 -0400

    date picker

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,12 +6,8 @@ const userSchema = new Schema({
     email: { type: String, unique: true },
     name: String,
     googleId: String,
-<<<<<<< HEAD
     DOB: Date,
     username: String
-=======
-    chart: Array
->>>>>>> 610f5a26aff9498e6ee4c3788012bf85abebad86
 })
 
 const User = model('User', userSchema)
