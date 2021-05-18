05f709fd9192b9f4f299931dcd84bfec88edc5bb
Tzikas <niko.tzikas@gmail.com>
2021-05-18 17:08:56 -0400

    okdokey

--- backend/app.js
+++ backend/app.js
@@ -9,43 +9,48 @@ const path = require('path');
 const passport = require('./config/passport');
 
 mongoose
-    .connect('mongodb+srv://team:chat2021@cluster0.2mhfj.mongodb.net/final', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(x => console.log(`Connected to ${x.connections[0].name}`))
-    .catch(() => console.error("Error connecting to Mongo"))
+  .connect('mongodb+srv://team:chat2021@cluster0.2mhfj.mongodb.net/final', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(x => console.log(`Connected to ${x.connections[0].name}`))
+  .catch(() => console.error("Error connecting to Mongo"))
 
+app.use(cors())
+
+console.log('hmm')
 const app_name = require('./package.json').name;
 const debug = require('debug')(
-    `${app_name}:${path.basename(__filename).split('.')[0]}`
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
 );
 app.use(express.json())
 
-app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:3000', 'http://nebulus-astrology.netlify.com'], //Swap this with the client url
-    })
-)
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://nebulus-astrology.netlify.com");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
+
+// app.use(cors({
+//     credentials: true,
+//     origin: ['http://localhost:3000', 'http://nebulus-astrology.netlify.com'], //Swap this with the client url
+//     })
+// )
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://nebulus-astrology.netlify.com");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// })
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use('/api', require('./routes/routes.js'))
 
 app.get('*', (req, res, next) => {
-    console.log('weird', req.headers.host, 'peach', req.url);
-  
-    if (req.headers.host.includes('heroku')) {
-      res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-    } else {
-      next();
-    }
-  });  
+  console.log('weird', req.headers.host, 'peach', req.url);
+
+  if (req.headers.host.includes('heroku')) {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  } else {
+    next();
+  }
+});
 
 
 module.exports = app;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,33 +4,24 @@ const router = express.Router()
 const Message = require('../models/Message')
 const User = require('../models/User')
 
-router.all('/', function(req, res, next) {
+router.all('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
-  
+
 router.post(`/add-message`, authorize, (req, res) => {
     let msg = req.body
     msg.ownerId = res.locals.user._id
     Message.create(msg).then(message => res.json(message))
 })
 
-router.get(`/getUser`, authorize, async (req, res) => {
-    //console.log("in get user after next", res.locals.user._id)
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
-        if (err) {
-          res.status(403).json(err);
-        } else {
-          User.findById(authData.user._id)
-            .then((user) => {
-              res.status(200).json(user);
-            })
-            .catch((err) => res.status(500).json(err));
-        }
-    });
-})
+router.get(`/get-user`, authorize, async (req, res) => {
 
+    console.log("in get user after next", res.locals.user._id)
+    let user = await User.findById(res.locals.user._id)
+    res.json(user)
+})
 
 
 
@@ -83,28 +74,53 @@ router.post('/addFriend', async (req, res) => {
 })
 
 router.get('/getFriends', authorize, async (req, res) => {
-    const me = await User.findById(res.locals.user._id)
-    const friends = await User.find({ "googleId": { "$in": me.friends } });
+    console.log(res.local)
+    // const me = await User.findById(res.locals.user._id)
+    // const friends = await User.find({ "googleId": { "$in": me.friends } });
 
-    res.json(friends);
+    // res.json(friends);
 })
+
+// function authorize(req, res, next) {
+//     console.log('monkey in the mittle', req.headers)
+//     const bearerHeader = req.headers['authorization'];
+//     // Check if bearer is undefined
+//     if (typeof bearerHeader !== 'undefined') {
+//         // Split at the space
+//         const bearer = bearerHeader.split(' ');
+//         // Get token from array
+//         const bearerToken = bearer[1];
+//         // Set the token
+//         req.token = bearerToken;
+//         // Next middleware
+//         next();
+//     } else {
+//         // Forbidden
+//         res.status(403); //.json({err:'not logged in'});
+//     }
+// }
+
 
 function authorize(req, res, next) {
     console.log('monkey in the mittle', req.headers)
-    const bearerHeader = req.headers['authorization'];
-    // Check if bearer is undefined
-    if (typeof bearerHeader !== 'undefined') {
-    // Split at the space
-        const bearer = bearerHeader.split(' ');
-    // Get token from array
-        const bearerToken = bearer[1];
-    // Set the token
-        req.token = bearerToken;
-    // Next middleware
-        next();
+    if (req.headers.authorization) {
+        let token = req.headers.authorization.split(' ')[1]
+        console.log(token)
+        jwt.verify(token, 'secret key', async (err, data) => {
+            if (!err) {
+                console.log(data)
+                res.locals.user = data.user
+                next()
+            } else {
+                console.error(err)
+                res.json({ err })
+            }
+        })
     } else {
-    // Forbidden
-        res.status(403); //.json({err:'not logged in'});
-  }
+        res.status(403).json({ message: 'You dont have no token' })
+    }
 }
+
+
+
 module.exports = router
