1cd4a02acbef886d274822053cce6c0cbd2ef06e
vlee13 <vlee13@gmail.com>
2021-05-20 16:02:53 -0400

    pulling

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
 
 mongoose
-  .connect('mongodb+srv://team:chat2021@cluster0.2mhfj.mongodb.net/final', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(x => console.log(`Connected to ${x.connections[0].name}`))
-  .catch(() => console.error("Error connecting to Mongo"))
+  .connect("mongodb+srv://team:chat2021@cluster0.2mhfj.mongodb.net/final", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => console.log(`Connected to ${x.connections[0].name}`))
+  .catch(() => console.error("Error connecting to Mongo"));
 
-
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'http://nebulus-astrology.netlify.com'], //Swap this with the client url
-    })
-)
+    origin: ["http://localhost:3000", "http://nebulus-astrology.netlify.com"], //Swap this with the client url
+  })
+);
 
+app.use(express.json());
 
-app.use(express.json())
+app.use("/api", require("./routes/routes.js"));
 
-
-app.use('/api', require('./routes/routes.js'))
-
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -33,9 +34,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('*', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../frontend/public/index.html'));
+app.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
 });
-
 
 module.exports = app;
