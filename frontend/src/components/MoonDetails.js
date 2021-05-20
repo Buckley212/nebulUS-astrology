c334c805381af8280105fd83ff883088e680aeb5
Ashtyn Ce <ashtyncza@gmail.com>
2021-05-20 19:21:33 -0400

    thursdayy

--- frontend/src/components/MoonDetails.js
+++ frontend/src/components/MoonDetails.js
@@ -5,8 +5,27 @@ import TheContext from "../services/TheContext";
 function MoonDetails(props) {
   const { user, setUser } = useContext(TheContext);
 
-  console.log(signs[0].Moon);
-  return <div>The Moon Sign</div>;
+  console.log(signs[0].Moon[user?.moon]?.Summary);
+
+  // return <div>The Moon Sign</div>;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: "20px",
+      }}
+    >
+      <h1>The Moon Sign:</h1>
+      <div>
+        <div>Summary: {signs[0].Moon[user?.moon]?.Summary}</div>
+      </div>
+    </div>
+
+  )
 }
 
 export default MoonDetails;
